5018a0442f46d0dc0272834bb7c5e8710028d73b
Andrew Stevens <andrew@andrewstevens.consulting>
2019-10-18 15:45:59 -0400

    Update replace_tl_files.sql

--- replace_tl_files.sql
+++ replace_tl_files.sql
@@ -2,6 +2,7 @@ UPDATE `tl_files` SET `path` = REPLACE(`path`, 'tl_files/', 'files/');
 UPDATE `tl_module` SET `html` = REPLACE(`html`, 'tl_files/', 'files/');
 UPDATE `tl_content` SET `text` = REPLACE(`text`, 'tl_files/', 'files/');
 UPDATE `tl_content` SET `html` = REPLACE(`html`, 'tl_files/', 'files/');
+UPDATE `tl_content` SET `url` = REPLACE(`url`, 'tl_files/', 'files/');
 UPDATE `tl_style` SET `bgimage` = REPLACE(`bgimage`, 'tl_files/', 'files/');
 UPDATE `tl_style` SET `liststyleimage` = REPLACE(`liststyleimage`, 'tl_files/', 'files/');
 UPDATE `tl_form_field` SET `text` = REPLACE(`text`, 'tl_files/', 'files/');
