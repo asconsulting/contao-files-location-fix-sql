140f01827a5a06755185a180d5aa5f00a2acd8e2
Andrew Stevens <andrew@andrewstevens.consulting>
2021-05-13 14:19:44 -0400

    Update replace_tl_files.sql

--- replace_tl_files.sql
+++ replace_tl_files.sql
@@ -3,6 +3,7 @@ UPDATE `tl_module` SET `html` = REPLACE(`html`, 'tl_files/', 'files/');
 UPDATE `tl_content` SET `text` = REPLACE(`text`, 'tl_files/', 'files/');
 UPDATE `tl_content` SET `html` = REPLACE(`html`, 'tl_files/', 'files/');
 UPDATE `tl_content` SET `url` = REPLACE(`url`, 'tl_files/', 'files/');
+UPDATE `tl_content` SET `imageUrl` = REPLACE(`imageUrl`, 'tl_files/', 'files/');
 UPDATE `tl_style` SET `bgimage` = REPLACE(`bgimage`, 'tl_files/', 'files/');
 UPDATE `tl_style` SET `liststyleimage` = REPLACE(`liststyleimage`, 'tl_files/', 'files/');
 UPDATE `tl_form_field` SET `text` = REPLACE(`text`, 'tl_files/', 'files/');
